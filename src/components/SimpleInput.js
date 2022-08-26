3f68486194dc1960a870e38b68123e8868c813a5
SpencerTower <spencerjtower1@gmail.com>
2022-08-26 12:38:50 -0700

    adds state to check input validity, dynamically sets css class based on input validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from 'react';
 const SimpleInput = (props) => {
     const nameInputRef = useRef();
     const [enteredName, setEnteredName] = useState('');
+    const [enteredNameValid, setEnteredNameValid] = useState(true);
 
     const nameInputChangeHandler = (event) => {
         setEnteredName(event.target.value);
@@ -13,8 +14,11 @@ const SimpleInput = (props) => {
 
         // exit function if user input is empty on submit
         if (enteredName.trim() === '') {
+            setEnteredNameValid(false);
             return;
         }
+
+        setEnteredNameValid(true);
 
         console.log('enteredName', enteredName);
         console.log('nameInputRef', nameInputRef);
@@ -24,9 +28,13 @@ const SimpleInput = (props) => {
         setEnteredName('');
     };
 
+    const nameInputClasses = enteredNameValid
+        ? 'form-control'
+        : 'form-control invalid';
+
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
+            <div className={nameInputClasses}>
                 <label htmlFor="name">Your Name</label>
                 <input
                     ref={nameInputRef}
@@ -34,6 +42,9 @@ const SimpleInput = (props) => {
                     id="name"
                     onChange={nameInputChangeHandler}
                 />
+                {!enteredNameValid && (
+                    <p className={'error-text'}>Name must not be empty.</p>
+                )}
             </div>
             <div className="form-actions">
                 <button>Submit</button>
